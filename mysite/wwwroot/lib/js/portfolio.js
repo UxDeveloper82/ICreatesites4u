bd974b5485e59a1c785b931d38db57682d4f939a
UxDeveloper82 <electronicsmf@aol.com>
2021-06-22 07:02:38 -0400

    Add project files.

---
+++ mysite/wwwroot/lib/js/portfolio.js
@@ -0,0 +1,104 @@
+﻿// Declare Values
+const sectionCard = document.querySelector('#card');
+const searchBar = document.getElementById('searchBar');
+const container = document.querySelector('.btn-container');
+const pagination_element = document.getElementById('pagination');
+
+let portCharacters = [];
+let current_page = 1;
+let rows = 5;
+
+
+//load items
+window.addEventListener('DOMContentLoaded', function (e) {
+    displayMenuButtons(portCharacters);
+});
+
+//Search Function
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
+    const filteredPorts = portsCharacters.filter(portfolios => {
+        return portfolios.name.toLowerCase().includes(searchString) ||
+               portfolios.type.toLowerCase().includes(searchString);
+    });
+    displayMenuItems(filteredPorts);
+});
+
+// Fetch the items 
+const loadPorts = async () => {
+    try {
+        const resp = await fetch('/api/portfolio');
+        portsCharacters = await resp.json();
+        displayMenuItems(portsCharacters);
+        console.log(portsCharacters);
+    } catch (err) {
+        console.log(err);
+    }
+};
+loadPorts();
+
+//Display items to screen
+const displayMenuItems = (portfolios) => {
+    const htmlString = portfolios.map((portfolios) => {
+        return ` 
+               <div class="col-lg-4 item ${portfolios.type}">
+                            <div class="card">
+                                <div class="card-head">
+                                    <img src="/Image/${portfolios.portfolioPhoto}" alt="" class="img-fluid card-img">
+                                    <div class="card-overlay"><h2>${portfolios.name}</h2></div>
+                                    <div class="card-hover">
+                                        <h2>Language: ${portfolios.language}</h2>
+                                        <p>Details: ${portfolios.detail}</p>
+                                    </div>
+                                </div>
+                                <div class="card-body text-center">
+                                    <h4 class="title">${portfolios.name}</h4>
+                                    <a href="${portfolios.link}" class="btn btn-lg card-btn">Goto Page</a>
+                                </div>
+                            </div>
+                    </div>
+               `;
+    }).join('');
+    sectionCard.innerHTML = htmlString;
+};
+
+
+function displayMenuButtons() {
+    const filterBtns = document.querySelectorAll('.filter-btn');
+    //filter items
+    filterBtns.forEach(function (btn) {
+        btn.addEventListener('click', function (e) {
+            const type = e.currentTarget.dataset.id;
+            const menuType = portsCharacters.filter(function (port) {
+                console.log(port.type);
+                if (port.type === type) {
+                    return port;
+                }
+            });
+            if (type === 'all') {
+                displayMenuItems(portsCharacters);
+            } else {
+                displayMenuItems(menuType);
+            }
+        })
+    })
+}
+displayMenuButtons();
+
+
+$(document).ready(function () {
+    $('.card').mouseenter(function () {
+        $(this).find('.card-overlay').css({ 'top': '-100%' });
+        $(this).find('.card-hover').css({ 'top': '0' });
+    }).mouseleave(function () {
+        $(this).find('.card-overlay').css({ 'top': '0' });
+        $(this).find('.card-hover').css({ 'top': '100%' });
+    });
+})
+loadPorts();
+
+const card = document.querySelector(".card");
+card.addEventListener('mouseenter', function (e) {
+    const itemEnter = e.target
+    console.log(itemEnter);
+});
---
+++ mysite/wwwroot/lib/js/script.js
@@ -0,0 +1,116 @@
+﻿/*!
+    * Start Bootstrap - Resume v6.0.1 (https://startbootstrap.com/template-overviews/resume)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+    */
+(function ($) {
+    "use strict"; // Start of use strict
+    // Preloader js    
+    $(window).on('load', function () {
+        $('.preloader').fadeOut(700);
+    });
+    // Background-images
+    $('[data-background]').each(function () {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    //Hero Slider
+    $('.hero-slider').slick({
+        autoplay: true,
+        infinite: true,
+        arrows: true,
+        prevArrow: '<button type=\'button\' class=\'prevArrow\'></button>',
+        nextArrow: '<button type=\'button\' class=\'nextArrow\'></button>',
+        dots: false,
+        autoplaySpeed: 7000,
+        pauseOnFocus: false,
+        pauseOnHover: false
+    });
+    $('.hero-slider').slickAnimation();
+    //  Count Up
+    function counter() {
+        var oTop;
+        if ($('.count').length !== 0) {
+            oTop = $('.count').offset().top - window.innerHeight;
+        }
+        if ($(window).scrollTop() > oTop) {
+            $('.count').each(function () {
+                var $this = $(this),
+                    countTo = $this.attr('data-count');
+                $({
+                    countNum: $this.text()
+                }).animate({
+                    countNum: countTo
+                }, {
+                    duration: 1000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                    }
+                });
+            });
+        }
+    }
+    $(window).on('scroll', function () {
+        counter();
+    });
+
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (
+            location.pathname.replace(/^\//, "") ==
+            this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate(
+                    {
+                        scrollTop: target.offset().top,
+                    },
+                    1000,
+                    "easeInOutExpo"
+                );
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function () {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#sideNav",
+    });
+})(jQuery); // End of use strict
+
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(`.box-shadow, .testimonials,
+            .elementor, #banner,
+            #contact, #tabs-container,
+            #tab-heading, .app__img,
+            .contact__data, .contact__button,
+            .footer__content`, {
+    interval: 200
+})
+
